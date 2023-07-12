d7ecba5d384d15c248527091dc8a177b60db03ab
Albert <albert@bitandpay.com>
2023-07-12 21:12:34 +0300

    feat(products): add product card

---
+++ src/app/pages/products-list/card/card.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {productMock} from 'src/app/shared/products/product.mock';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+    protected product = productMock;
+
+    protected buy(e: Event) {
+        e.stopPropagation();
+    }
+}
