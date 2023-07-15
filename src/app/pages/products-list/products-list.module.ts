8fb2a031d345308f96a18c196312d6bbaae12dcb
Qusz <de.gorchakov@gmail.com>
2023-07-15 13:12:56 +0300

    feat: import card module

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProductsListComponent} from './products-list.component';
+// import {CardComponent} from './card/card.component';
+
+import {CardModule} from './card/card.module';
 
 @NgModule({
+    // declarations: [ProductsListComponent, CardComponent],
     declarations: [ProductsListComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, CardModule],
     exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
