d6898fe4ee28e6f176106771932b85dda6a2ca70
Qusz <de.gorchakov@gmail.com>
2023-07-16 17:35:47 +0300

    feat: implement add to card logic

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
+import {IProduct} from 'src/app/shared/products/product.interface';
 import {Component} from '@angular/core';
+import {productsMock} from 'src/app/shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -7,8 +9,15 @@ import {Component} from '@angular/core';
     // encapsulation: ViewEncapsulation.Emulated,
 })
 export class ProductsListComponent {
+    readonly products: IProduct[] = productsMock;
+
     onCardClick() {
         // eslint-disable-next-line no-console
         console.log('Card click');
+    }
+
+    handleAddToCart(id: string) {
+        // eslint-disable-next-line no-console
+        console.log(`Id ${id} added to cart`);
     }
 }
