948d7505944d7dc1e9f0c647f4f027b9b5e75134
Musin Dinislam <MusinDi@inmeta.ru>
2023-07-19 09:21:22 +0500

    feat: adding Input and Output

--- src/app/pages/products-list/card/card.module.ts
+++ src/app/pages/products-list/card/card.module.ts
@@ -3,11 +3,18 @@ import {CommonModule} from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {CardComponent} from './card.component';
 
 @NgModule({
     declarations: [CardComponent],
-    imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
+    imports: [
+        CommonModule,
+        MatCardModule,
+        MatButtonModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+    ],
     exports: [CardComponent],
 })
 export class CardModule {}
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,13 @@
 import {Component} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
+import {productsMock} from 'src/app/shared/products/products.mock';
+
+export interface IProductInCart {
+    productId: IProduct['_id'];
+    count: number;
+}
+
+export type TCart = IProductInCart[];
 
 @Component({
     selector: 'app-products-list',
@@ -7,8 +16,25 @@ import {Component} from '@angular/core';
     // encapsulation: ViewEncapsulation.Emulated,
 })
 export class ProductsListComponent {
+    readonly products: IProduct[] = productsMock;
+    readonly cart: TCart = [];
+
     onCardClick() {
         // eslint-disable-next-line no-console
         console.log('Card click');
+    }
+
+    onBuyProduct(id: IProduct['_id']) {
+        const productInCart = this.cart.find(prod => prod.productId === id);
+
+        if (productInCart) {
+            productInCart.count++;
+
+            return;
+        }
+
+        this.cart.push({productId: id, count: 1});
+
+        console.info(this.cart);
     }
 }
