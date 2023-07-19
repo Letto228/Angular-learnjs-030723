828dd6c771e87607c973dce25f2fc96972efc317
Musin Dinislam <MusinDi@inmeta.ru>
2023-07-19 08:21:04 +0500

    refactor: card.component 1. Биндинг alt-img 2. Исправлено rule .eslint.js 3. Отрефакторены методы компонента 4. productMockService вынесен в родительский компонент 5. Добавлен Input

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,71 +1,37 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {IProduct} from 'src/app/shared/products/product.interface';
-import {Subscription} from 'rxjs';
-import {ProductMockService} from './product-mock.service';
 
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.css'],
 })
-export class CardComponent implements OnInit, OnDestroy {
-    productModel: IProduct | undefined;
+export class CardComponent {
+    @Input() product!: IProduct;
     currentImageNumber = 0;
-    private readonly subscriptions: Subscription[] = [];
-
-    constructor(private readonly _productMockService: ProductMockService) {}
-
-    ngOnInit() {
-        const subscription = this._productMockService.getMockData().subscribe({
-            next: (mockData: IProduct) => {
-                this.productModel = mockData;
-            },
-            error: (error: unknown) => {
-                console.error(error);
-            },
-        });
-
-        this.subscriptions.push(subscription);
-    }
-
-    ngOnDestroy() {
-        this.subscriptions.forEach(sub => sub.unsubscribe());
-    }
 
     nextImage(): void {
-        if (!this.productModel) {
-            return;
-        }
+        const lastImageIndex = this.product.images.length - 1;
+        const firstImageIndex = 0;
+        const isLastMessageIndex = this.currentImageNumber === lastImageIndex;
 
-        if (this.currentImageNumber === this.productModel.images.length - 1) {
-            this.currentImageNumber = 0;
-
-            return;
-        }
-
-        this.currentImageNumber += 1;
+        this.currentImageNumber = isLastMessageIndex
+            ? firstImageIndex
+            : this.currentImageNumber + 1;
     }
 
     previousImage(): void {
-        if (!this.productModel) {
-            return;
-        }
+        const lastImageIndex = this.product.images.length - 1;
+        const firstImageIndex = 0;
+        const isFirstMessageIndex = this.currentImageNumber === firstImageIndex;
 
-        if (this.currentImageNumber === 0) {
-            this.currentImageNumber = this.productModel.images.length - 1;
-
-            return;
-        }
-
-        this.currentImageNumber -= 1;
+        this.currentImageNumber = isFirstMessageIndex
+            ? lastImageIndex
+            : this.currentImageNumber - 1;
     }
 
     returnCurrentImageSrc(): string {
-        if (!this.productModel) {
-            return '0';
-        }
-
-        return this.productModel.images[this.currentImageNumber].url;
+        return this.product.images[this.currentImageNumber].url;
     }
 
     buyProduct(): void {
--- src/app/pages/products-list/card/card.module.ts
+++ src/app/pages/products-list/card/card.module.ts
@@ -4,12 +4,10 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
 import {CardComponent} from './card.component';
-import {ProductMockService} from './product-mock.service';
 
 @NgModule({
     declarations: [CardComponent],
     imports: [CommonModule, MatCardModule, MatButtonModule, MatDividerModule],
-    providers: [ProductMockService],
     exports: [CardComponent],
 })
 export class CardModule {}
