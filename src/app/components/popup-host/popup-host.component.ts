dbcd844ce8f7af9cb5f3dc226c6c7ec867502ddd
Musin Dinislam <MusinDi@inmeta.ru>
2023-07-31 19:20:50 +0500

    fix: 1. popup host overlay changed with HostBinding 2. adding [ngTemplateOutlet]

--- src/app/components/popup-host/popup-host.component.ts
+++ src/app/components/popup-host/popup-host.component.ts
@@ -1,44 +1,14 @@
-import {
-    Component,
-    ElementRef,
-    Input,
-    OnChanges,
-    Renderer2,
-    SimpleChanges,
-    TemplateRef,
-    ViewChild,
-    ViewContainerRef,
-} from '@angular/core';
+import {Component, HostBinding, Input, TemplateRef} from '@angular/core';
 
 @Component({
     selector: 'app-popup-host',
     templateUrl: './popup-host.component.html',
     styleUrls: ['./popup-host.component.css'],
 })
-export class PopupHostComponent implements OnChanges {
+export class PopupHostComponent {
     @Input() templateRef: TemplateRef<unknown> | null = null;
 
-    @ViewChild('viewport', {read: ViewContainerRef, static: true})
-    private readonly viewport!: ViewContainerRef;
-
-    constructor(private readonly elementRef: ElementRef, private readonly renderer: Renderer2) {}
-
-    ngOnChanges({templateRef}: SimpleChanges): void {
-        if (templateRef) {
-            this.insertView();
-        }
-    }
-
-    insertView() {
-        this.viewport.clear();
-
-        if (this.templateRef) {
-            this.renderer.setStyle(this.elementRef.nativeElement, 'display', 'flex');
-            this.viewport.createEmbeddedView(this.templateRef);
-
-            return;
-        }
-
-        this.renderer.setStyle(this.elementRef.nativeElement, 'display', 'none');
+    @HostBinding('style.display') get show() {
+        return this.templateRef !== null ? 'flex' : 'none';
     }
 }
